412841c30fef5026c3df5a5310ad0b2fb9d4cafc
morganhah99 <mjh58@njit.edu>
2024-05-31 17:56:54 -0400

    set up navigation

---
+++ app/src/main/java/com/example/spacex/ui/compose/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.spacex.ui.compose
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+
+    Text(
+        text="hello world"
+    )
+}
